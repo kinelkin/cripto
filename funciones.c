cd905d9711036f8d3f06720131f844dafa9a9624
David Quintana <david.quintanar@estudiante.uam.es>
2020-10-21 11:28:51 +0200

    Corregido error de valgrind en Euclidean, empezando afin

--- funciones.c
+++ funciones.c
@@ -6,10 +6,12 @@ void Euclidean(mpz_t resultado, mpz_t a, mpz_t b){
 
   if(mpz_sgn(a)==0){
     mpz_set(resultado,b);
+    mpz_clear(modulo);
     return;
   }
   mpz_mod(modulo,b,a);
   Euclidean(resultado,modulo,a);
+  mpz_clear(modulo);
 }
 
 void euclideanExtended(mpz_t resultado, mpz_t a, mpz_t b){
@@ -61,5 +63,63 @@ void euclideanExtended(mpz_t resultado, mpz_t a, mpz_t b){
 
 }
 
+/*void cifrarAfin(mpz_t a, mpz_t b, mpz_t m, char* mensaje){
+  mpz_t total,resultado;
+  int i;
+  char* textoPlano;
+  char textoCifrado, x;
+
+  mpz_init(resultado);
+  mpz_init(total);
 
 
+  euclideanExtended(resultado,a,m);
+
+  if (mpz_cmp_ui(resultado,1)){
+    printf("No existe inverso multiplicativo de a y por tanto no podemos cifrar");
+    return;
+  }
+
+  printf("hola manuelo");
+  textoPlano = (char*)malloc(sizeof(char)*5);
+  printf("hola manuelo");
+  strcpy(textoPlano,mensaje);
+
+  for (i=0; i<=strlen(textoPlano); i++){
+    x = toupper(textoPlano[i]) - 65;
+    mpz_mul_ui(a,a,x);
+    mpz_add(a,a,b);
+    mpz_mod(total,a,m);
+    mpz_add_ui(total,total,65);
+  }
+
+  gmp_printf("%Zd \n", total);
+
+
+  mpz_clear(resultado);
+  mpz_clear(total);
+
+  return;
+
+}
+
+
+int main(){
+    mpz_t a,b,m;
+
+    char mensaje[4] = "hola";
+
+    mpz_init(a);
+    mpz_init(b);
+    mpz_init(m);
+
+    mpz_set_str (a,"9233473",10);
+    mpz_set_str (b,"1213232",10);
+    mpz_set_str (m,"3",10);
+    cifrarAfin(a,b,m,mensaje);
+
+    mpz_clear(a);
+    mpz_clear(b);
+    mpz_clear(m);
+
+}*/
--- funciones.h
+++ funciones.h
@@ -1,10 +1,14 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <gmp.h>
+#include <ctype.h>
 
 void Euclidean(mpz_t resultado, mpz_t a, mpz_t b);
 void euclideanExtended(mpz_t resultado, mpz_t a, mpz_t b);
+void cifrarAfin(mpz_t a, mpz_t b, mpz_t m, char* mensaje);
 
 #endif
